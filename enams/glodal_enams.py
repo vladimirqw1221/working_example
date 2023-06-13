d807e670db19f8c4f99a4fce74fe971f0bc3664b
Vladimir <111503066+vladimirqw1221@users.noreply.github.com>
2023-06-14 00:48:27 +0400

    new value

--- enams/glodal_enams.py
+++ enams/glodal_enams.py
@@ -4,3 +4,4 @@ from enum import Enum
 class GlodalEnums(Enum):
     WRONG_ERROR_URL = "Issue! assert test Filed"
     WRONG_ERROR_WORD = "Issue! assert word test Filed"
+    WRONG_VALUE_NOT_CORRECT="Issue! Not correct value"
