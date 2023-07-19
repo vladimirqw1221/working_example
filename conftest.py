b846b11e987d2828e3ee1cf271cbaaf7fab4891f
Vladimir <111503066+vladimirqw1221@users.noreply.github.com>
2023-07-19 23:27:35 +0400

    new configuration for conftest dedug mode

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
@@ -10,13 +12,17 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.close()
+    driver.quit()
+
+
+@pytest.fixture(scope='function')
+def driver_debug():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     print("\nLaunch test. . . ")
     yield driver
     driver.close()
     driver.quit()
     print("\nFinish test...")
-
-
-
-
-
